a272e9783120a6f3f2c8070808c89eb7df62ec0b
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2022-04-03 16:31:17 +0300

    Update favorites_page.dart

--- lib/screens/favorites_page.dart
+++ lib/screens/favorites_page.dart
@@ -1,21 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:plypicker/helpers/add_favorites.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/favorite_products.dart';
 
 class FavoritesPage extends StatelessWidget {
   const FavoritesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final helperFunctions = AddFavorites();
+    final favorites = Provider.of<FavoriteProducts>(context).favorites;
+    final unfavoriteFunction = Provider.of<FavoriteProducts>(context);
+    final deviceSize = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: helperFunctions.favorites.isNotEmpty
+      body: favorites.isEmpty
           ? const Center(child: Text('No favorites yet'))
           : ListView.separated(
-              itemCount: 2,
-              separatorBuilder: (context, index) => const Divider(),
-              itemBuilder: ((context, index) => const ListTile(
-                    leading: CircleAvatar(),
+              itemCount: favorites.length,
+              separatorBuilder: (context, index) => const Divider(thickness: 2),
+              itemBuilder: ((context, index) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          height: deviceSize.height * .12,
+                          width: deviceSize.width * .18,
+                          child: Image.network(
+                            favorites[index]['product_image'],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(favorites[index]['product_name']),
+                            Text(
+                              favorites[index]['brand'],
+                              style: const TextStyle(
+                                fontSize: 13,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Text('Rs.${favorites[index]['discounted_price']}'),
+                          ],
+                        ),
+                        const SizedBox(),
+                        GestureDetector(
+                          child: const Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                            size: 30,
+                          ),
+                          onTap: () => unfavoriteFunction
+                              .removeFromFavorites(favorites[index]),
+                        ),
+                      ],
+                    ),
                   )),
             ),
     );
