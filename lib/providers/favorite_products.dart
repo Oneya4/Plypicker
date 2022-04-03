86047b84f2843789a1acf88e2feec242551c057a
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2022-04-03 16:31:11 +0300

    Create favorite_products.dart

---
+++ lib/providers/favorite_products.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FavoriteProducts with ChangeNotifier {
+  final List<Map<String, dynamic>> favorites = [];
+
+  void addToFavorites(Map<String, dynamic> product) {
+    favorites.add(product);
+
+    print(favorites);
+    notifyListeners();
+  }
+
+  void removeFromFavorites(Map<String, dynamic> product) {
+    favorites.remove(product);
+    notifyListeners();
+  }
+}
