e98497a12947e444f5a8cfc115f6fae6ff8bfc0c
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2022-04-03 16:31:30 +0300

    Create nav_bar.dart

---
+++ lib/widgets/nav_bar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import '/screens/screens.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({Key? key}) : super(key: key);
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  final List<Widget> _pages = [
+    const HomePage(),
+    const FavoritesPage(),
+  ];
+
+  int _currentIndex = 0;
+
+  void changePage(int selectedIndex) {
+    setState(() {
+      _currentIndex = selectedIndex;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: const Text(
+            'PLYPICKER',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        body: IndexedStack(children: [_pages[_currentIndex]]),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_border_sharp),
+              label: 'Favorites',
+            ),
+          ],
+          currentIndex: _currentIndex,
+          onTap: changePage,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          iconSize: 32,
+        ),
+      ),
+    );
+  }
+}
